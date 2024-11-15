8c32db3fc69da48d073826f05f363645246a8924
Rennam Victor Cabral de Faria <98751401+RennamFaria@users.noreply.github.com>
2024-11-15 09:34:17 -0300

    Aluno comments

--- FrontEnd/Projects/Library/scripts/aluno.js
+++ FrontEnd/Projects/Library/scripts/aluno.js
@@ -1,37 +1,59 @@
 const urlApi = 'http://localhost:3000/alunos';
+const resultDiv = document.getElementById('result');
 
-async function cadastrarAlunos(event) {
-    //remover o comportamento de recarregar a pagina ao dar submit
-    event.preventDefault();
 
-    const nome = document.getElementById('nome').value;
-    const CPF = document.getElementById('cpf').value;
-    const email = document.getElementById('email').value;
-    const tel = document.getElementById('tel').value;
-    const date = document.getElementById('date').value;
-    
-    const aluno = {
-        nome,
-        CPF,
-        email,
-        tel,
-        date
-    };
+async function cadastraAlunos(event) {
+  // Removo o comportamento de carregar a pagina
+  event.preventDefault();
 
-    // Enviar uma requisicao para o link/banco de dados
-    const request = new Request(urlApi, {
-        method: 'POST',
-        body: JSON.stringify(aluno),
-        headers: new Headers({
-            'Content-Type': 'aplication/json'
-        })
-    });
+  // Buscar o que usuario digitou no formulario
+  const nome = document.getElementById('nome').value;
+  const cpf = document.getElementById('cpf').value;
+  const email = document.getElementById('email').value;
+  const tel = document.getElementById('tel').value;
+  const date = document.getElementById('date').value;
 
-    const response = await fetch(request);
-    console.log(response);
+  const aluno = {
+    nome,
+    cpf,
+    email,
+    tel,
+    date
+  }
 
-    //C      R    U         D
-    //Create Read Update    Delete
+  const request = new Request(urlApi, {
+    method: 'POST',
+    body: JSON.stringify(aluno),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
 
-    //POST  GET  PUT/PATH   DELETE
+  const response = await fetch(request)
+  const data = await response;
+
+  alert('Cadastrado com sucesso !')
 }
+//Create    Read    Update    Delete
+//C         R       U         D
+
+//Post      GET   PATCH/PUT  DELETE
+
+async function getAlunos() {
+  const response = await fetch(urlApi);
+  const result = await response.json();
+
+  result.forEach(element => {
+    resultDiv.insertAdjacentHTML('beforeend',
+          `
+              <tr>
+                  <td>${element.nome}</td>
+                  <td>${element.cpf}</td>
+                  <td>${element.email}</td>
+              </tr>
+          `
+      )
+  });
+};
+
+getAlunos()
